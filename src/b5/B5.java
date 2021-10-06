26e473610866454bf0ca89338bd5df5d3f269c19
DELL <DELL@DESKTOP-OGRVV23>
2021-10-06 14:34:03 +0700

    b5

---
+++ src/b5/B5.java
@@ -0,0 +1,15 @@
+
+package b5;
+
+
+public class B5 {
+
+    public static void main(String[] args) {
+        int i;
+       for ( i=-1;i>=-10;i--)
+       {
+           System.out.println(i);
+       }
+    }
+    
+}
